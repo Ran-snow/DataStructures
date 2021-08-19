468b9adf0c37505b89f8a96078e6430731193689
Tom <18158510+Ran-snow@users.noreply.github.com>
2021-08-19 23:46:56 +0800

    BobbleSort

--- BobbleSort/Program.cs
+++ BobbleSort/Program.cs
@@ -1,13 +1,39 @@
 ﻿using System;
 
+using Common;
+
 namespace BobbleSort
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] data = { 4, 5, 3, 6, 2, 5, 1 };
-            Console.WriteLine("Hello World!");
+            DataHelper.Show(BobbleSort(DataHelper.Data));
+
+            Console.WriteLine("Nice!");
         }
+
+        /// <summary>
+        /// 冒泡排序
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        static int[] BobbleSort(int[] data)
+        {
+            for (int i = data.Length - 1; i > -1; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (data[j] > data[j + 1])
+                    {
+                        DataHelper.Swap(ref data[j], ref data[j + 1]);
+                    }
+                }
+            }
+
+            return data;
+        }
+
+
     }
 }
---
+++ Common/DataHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Common
+{
+    public static class DataHelper
+    {
+        static int[] data =new int[7] { 4, 5, 3, 6, 2, 5, 1 };
+        public static int[] Data 
+        { 
+            get 
+            {
+                Show(data);
+                return data; 
+            } 
+        }
+
+        public static void Swap(ref int a, ref int b)
+        {
+            //int temp = a;
+            //a = b;
+            //b = temp;
+
+            a ^= b;
+            b ^= a;
+            a ^= b;
+        }
+
+        public static void Show(int[] a)
+        {
+            foreach (var item in a)
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine();
+        }
+    }
+}
